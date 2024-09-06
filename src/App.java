f2888a0812ece688a960d10901ca6f9d21590b1d
OpenTheSiloDooors <jacob.espinoza@malad.us>
2024-09-06 13:14:26 -0700

    Initial commit, again

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+    // Loop from 1 to 100
+    for (int i = 1; i <= 100; i++) {
+    
+    // If the number is even, print even
+    if (i % 2 == 0) {
+        
+        System.out.println(i + " is even");
+        } else
+
+        // If it's not even, print odd
+        System.out.println(i + " is odd");
+        }
+    }
+}
