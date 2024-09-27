bf1bca2a76891ef82644167ab80717f7fb5ded0d
OpenTheSiloDooors <jacob.espinoza@malad.us>
2024-09-26 17:50:21 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args) {
-    int sum = 0;
+    //Created by Jacob Aaron Espinoza. Prints a loop from 1 to 100, declares whether they are even or odd, and adds all the numbers.
+    
+        int sum = 0; //initializing variable
     
     // Loop from 1 to 100
     for (int i = 1; i <= 100; i++) {
-        sum += i;
+        sum += i; // add all the numbers
 
-    
-    
     // If the number is even, print even
     if (i % 2 == 0) {
         
@@ -17,7 +17,7 @@ public class App {
         // If it's not even, print odd
         System.out.println(i + " is odd");
 
-    // Add all numbers
+    // Print all numbers and sum of all numbers
     System.out.println("The sum is " +sum);
         }
     }
