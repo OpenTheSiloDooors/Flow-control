0273ae880279cbebfc11990e997c2908125be7f2
OpenTheSiloDooors <jacob.espinoza@malad.us>
2024-09-26 17:38:50 -0700

    Add sum
    Adds the sum of every number

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
     public static void main(String[] args) {
+    int sum = 0;
+    
     // Loop from 1 to 100
     for (int i = 1; i <= 100; i++) {
+        sum += i;
+
+    
     
     // If the number is even, print even
     if (i % 2 == 0) {
@@ -11,6 +16,9 @@ public class App {
 
         // If it's not even, print odd
         System.out.println(i + " is odd");
+
+    // Add all numbers
+    System.out.println("The sum is " +sum);
         }
     }
 }
